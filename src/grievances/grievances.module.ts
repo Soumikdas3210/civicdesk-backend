c490065c5d1f52701c1cad617d068fb88561b1fb
taief <taiefsazzad@gmail.com>
2026-07-23 22:07:19 +0600

    notifications: add NotificationService with INV-11 unthrowable notify()

--- src/grievances/grievances.module.ts
+++ src/grievances/grievances.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GrievancesService } from './grievances.service';
 import { GrievancesController } from './grievances.controller';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
+  imports: [NotificationsModule],
   controllers: [GrievancesController],
   providers: [GrievancesService],
 })
--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Notification } from './entities/notification.entity';
+import { MailService } from '../mail/mail.service';
+import { NotificationType } from '../common/enums';
+
+export interface CreateNotificationInput {
+  userId: string;
+  type: NotificationType;
+  title: string;
+  body: string;
+  grievanceId?: string;
+}
 
 @Injectable()
-export class NotificationsService {}
+export class NotificationService {
+  private readonly logger = new Logger(NotificationService.name);
+
+  constructor(
+    @InjectRepository(Notification) private repo: Repository<Notification>,
+    private mailService: MailService,
+  ) {}
+
+  async notify(input: CreateNotificationInput): Promise<void> {
+    try {
+      await this.repo.save(this.repo.create(input));
+      await this.mailService.send(input);
+    } catch (err) {
+      this.logger.error(`Notification failed for user ${input.userId}`, err.stack);
+    }
+  }
+}
