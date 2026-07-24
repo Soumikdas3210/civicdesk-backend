042839cb4d97805f69581396ae0f8e0130ffc614
Soumik <soumikdas3210@gmail.com>
2026-07-24 11:56:35 +0600

    feat(grievances): AuditLog entity and AuditService, pulled forward from 4.7 as a 4.2 prerequisite

--- src/grievances/grievances.module.ts
+++ src/grievances/grievances.module.ts
@@ -3,11 +3,13 @@ import { GrievancesService } from './grievances.service';
 import { GrievancesController } from './grievances.controller';
 import { Grievance } from './entities/grievance.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuditLog } from './entities/audit-log.entity';
+import { AuditService } from './audit.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Grievance])],
+  imports: [TypeOrmModule.forFeature([Grievance, AuditLog])],
   controllers: [GrievancesController],
-  providers: [GrievancesService],
-  exports: [GrievancesService, TypeOrmModule],
+  providers: [GrievancesService, AuditService],
+  exports: [GrievancesService, AuditService, TypeOrmModule],
 })
 export class GrievancesModule {}
