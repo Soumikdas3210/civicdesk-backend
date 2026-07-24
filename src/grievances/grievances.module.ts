c4f00c83e497e13831e5d803fb4473f46c562a5f
Soumik <soumikdas3210@gmail.com>
2026-07-24 10:53:21 +0600

    feat(grievances): Grievance entity, complete in one commit per M4 ownership rule (4.1)

--- src/grievances/grievances.module.ts
+++ src/grievances/grievances.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GrievancesService } from './grievances.service';
 import { GrievancesController } from './grievances.controller';
+import { Grievance } from './entities/grievance.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Grievance])],
   controllers: [GrievancesController],
   providers: [GrievancesService],
+  exports: [GrievancesService, TypeOrmModule],
 })
 export class GrievancesModule {}
